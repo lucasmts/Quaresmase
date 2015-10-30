8c8201b30bd7a2ed75c866d5ede3afb9482a88e5
lucasmts <lukaosilveira77@gmail.com>
2015-10-30 14:49:30 -0200

    Update and rename .gitignore to .js

---
+++ .js
@@ -0,0 +1,76 @@
+(function ($) {
+
+    var self = {
+        vampetaseImg: [
+            'https://goo.gl/ACLQeF',
+            'https://goo.gl/nLhwGB',
+            'https://goo.gl/O2c6I6',
+            'https://goo.gl/8vQxEj',
+            'https://goo.gl/KV9iBx',
+            'https://goo.gl/R1EMq2',
+            'https://goo.gl/Om0zdl',
+            'https://goo.gl/bFb5VH',
+            'https://goo.gl/bcIXin',
+        ],
+        handleImages: function (lstImgs, time) {
+            $.each($('img'), function (i, item) {
+                //Skip if image is already replaced
+                if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                    var h = $(item).height();
+                    var w = $(item).width();
+
+                    //If image loaded
+                    if (h > 0 && w > 0) {
+                        self.handleImg(item, lstImgs);
+                    }
+                    else {
+                        //Replace when loaded
+                        $(item).load(function () {
+                            //Prevent 'infinite' loop
+                            if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                                self.handleImg(item, lstImgs);
+                            }
+                        });
+                    }
+                }
+            });
+
+            //Keep replacing
+            if (time > 0) {
+                setTimeout(function () { self.handleImages(lstImgs, time); }, time);
+            }
+        },
+        handleImg: function (item, lstImgs) {
+            $(item).error(function () {
+                //Handle broken imgs
+                self.handleBrokenImg(item, lstImgs);
+            });
+
+            self.setRandomImg(item, lstImgs);
+        },
+        setRandomImg: function (item, lstImgs) {
+            var h = $(item).height();
+            var w = $(item).width();
+            $(item).css('width', w + 'px').css('height', h + 'px');
+            $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+        },
+        handleBrokenImg: function (item, lstImgs) {
+
+            var brokenImg = $(item).attr('src');
+            var index = lstImgs.indexOf(brokenImg);
+            if (index > -1) {
+                lstImgs.splice(index, 1);
+            }
+            self.setRandomImg(item, lstImgs);
+        },
+    };
+
+    //Run on jQuery ready
+    $(function () {
+        self.handleImages(self.vampetaseImg, 3000);
+    });
+
+    //Set global variable
+    $.nCage = self;
+
+})(jQuery);
